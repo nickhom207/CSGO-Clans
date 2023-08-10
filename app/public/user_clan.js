87964a65c92da5da1a1cf8d85bd08be680ea1ff0
Joshua Jacob <joshua14jacob@gmail.com>
2023-08-10 11:12:25 -0400

    Update user clan page

--- app/public/user_clan.js
+++ app/public/user_clan.js
@@ -1,6 +1,8 @@
 
 
-let userID = "1001";
+let userID = "1000";
+var table = document.getElementById("clans");
+
 
 fetch(`/user_clan?userID=${userID}`).then((response) => {
     return response.json();
@@ -9,7 +11,7 @@ fetch(`/user_clan?userID=${userID}`).then((response) => {
     console.log(body);
 
     if(body.rows.length == 0){
-        document.getElementById("welcomeUser").textContent = "The user has not joine a clan yet.";
+        document.getElementById("welcomeUser").textContent = "The user has not joined a clan yet.";
         return;
     }
 
@@ -23,29 +25,23 @@ fetch(`/user_clan?userID=${userID}`).then((response) => {
 
     document.getElementById("welcomeUser").textContent = "Welcome   to your clan!";  
     // body.rows[0].username
-    console.log(body.rows.clans);
+    console.log(body.rows[0].clans);
 
     // display data
-    // for(let i = 0; i < body.rows.length; i++){
-    //     var row = document.createElement("tr");
-    //     var cell1 = document.createElement("td");
-    //     cell1.textContent = body.rows[i].title;
-    //     var cell2 = document.createElement("td");
-    //     cell2.textContent = body.rows[i].genre;
-    //     var cell3 = document.createElement("td");
-    //     if(body.rows[i].quality){
-    //         cell3.textContent = "yes";
-    //     }
-    //     else{
-    //         cell3.textContent = "no";
-    //     }
+    for(let i = 0; i < body.rows[0].clans.length; i++){
+        fetch(`/user_clan_detail?clanID=${i}`).then((response) => {
+            return response.json();
+        })
+        .then((body) => {
+            console.log(body);
+        });
+        var row = document.createElement("tr");
+        var cell1 = document.createElement("td");
+        cell1.textContent = body.rows[0].clans[i];
 
+        row.appendChild(cell1);
 
-    //     row.appendChild(cell1);
-    //     row.appendChild(cell2);
-    //     row.appendChild(cell3);
+        table.appendChild(row);
 
-    //     table.appendChild(row);
-
-    // }
+    }
 });
