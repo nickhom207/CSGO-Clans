eafd6fe6dfc5d4ed7142a98480f9765f92514ea9
Joshua Jacob <joshua14jacob@gmail.com>
2023-08-15 12:05:29 -0400

    Update user clan page

--- app/public/user_clan/script.js
+++ app/public/user_clan/script.js
@@ -1,6 +1,7 @@
 
 let steamID = "76561198025039301";
 var table = document.getElementById("clans");
+var clanList = document.getElementById("clanList");
 
 
 fetch(`/user-clan?steamID=${steamID}`).then((response) => {
@@ -27,12 +28,40 @@ fetch(`/user-clan?steamID=${steamID}`).then((response) => {
         })
         .then((body) => {
             // console.log(body.rows);
+            let button = document.createElement("button");
+            button.textContent = body.rows[0].clan_name;
+            button.onclick = function(){clanInfoFun(button.textContent)}
+
+
             var row = document.createElement("tr");
             var cell1 = document.createElement("td");
             cell1.textContent = body.rows[0].clan_name;
+            // add button into cell1
+
+            // cell1.innerHTML = "<button onclick='clanInfoFun(button.textContent)';>${body.rows[0].clan_name}</button>";
             row.appendChild(cell1);
 
             table.appendChild(row);
+
+
+            
+            // clanInfoFun(button.textContent);
+
+            clanList.appendChild(button);
+
         });
     }
 });
+
+function clanInfoFun(clanName){
+    console.log(clanName);
+    window.location.href = `/clan-info?clanName=${clanName}`;
+
+    // fetch(`/clan-info?clanName=${clanName}`).then((response) => {
+    //     return response.json();
+    // })
+    // .then((body) => {
+    //     console.log(body.rows);
+
+    // });
+}
