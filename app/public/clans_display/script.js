a1b43154d6be51700cbd83176eabfeb11dae4c7b
Joshua Jacob <joshua14jacob@gmail.com>
2023-08-29 12:09:03 -0400

    Updating the public clan pages

--- app/public/clans_display/script.js
+++ app/public/clans_display/script.js
@@ -13,13 +13,14 @@ fetch(`/all-public-clan-info`).then((response) => {
 
         let button1 = document.createElement("button");
         button1.textContent = body.rows[i].clan_name;
-        button1.onclick = function(){clanInfoFun(body.rows[i].clan_name)};
+        button1.onclick = function(){clanInfoFun(body.rows[i].unique_id)};
         // button.onclick = function(){clanInfoFun(button.textContent)}
         cell1.appendChild(button1);   
         row.appendChild(cell1);
 
         let button2 = document.createElement("button");
         button2.textContent = "Join";
+        button2.onclick = function(){clanJoinFun(body.rows[i].unique_id)};
         cell2.appendChild(button2);
         row.appendChild(cell2);
         table.appendChild(row);
@@ -29,6 +30,30 @@ fetch(`/all-public-clan-info`).then((response) => {
 
 });
 
-function clanInfoFun(clanName){
-    window.location.href = `/clan-pages?unique_id=${clanName}`;
+function clanInfoFun(unique_id){
+    window.location.href = `/clan-pages?unique_id=${unique_id}`;
+}
+
+function clanJoinFun(unique_id){
+    console.log(unique_id);
+    let info = JSON.stringify({"unique_id" : unique_id});
+    fetch("/join-clan", {method: "POST", headers: {"Content-Type": "application/json"}, body: info} 
+    ).then((response) => {
+        respCode = response.status;
+        return response.text();
+    }).then((body) => {
+        if (respCode != 200) {
+            console.log(body);
+            alert("You have already joined this clan!");
+            return;
+        }
+        alert("You have joined the clan!");
+            window.location.href = `/clan-pages?unique_id=${unique_id}`;
+
+    });
+}
+
+function callJoinClan(){
+    var privateClanUniqueId = document.getElementById("privateClanJoin").value;
+    clanJoinFun(privateClanUniqueId);
 }
--- app/server.js
+++ app/server.js
@@ -114,6 +114,7 @@ app.post("/create-clan", (req, res) => {
 });
 
 app.post("/join-clan", (req, res) => {
+    console.log("HII")
     let {unique_id} = req.body;
     let token = getToken(req.cookies);
 
